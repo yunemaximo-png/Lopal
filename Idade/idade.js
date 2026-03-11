8aa266a6ed359b8ec31cf5318eb3eb8dff5e7c3e
yunemaximo-png <yune270909@gmail.com>
2026-03-11 16:39:06 -0300

    Commit

--- Idade/idade.js
+++ Idade/idade.js
@@ -1,3 +1,7 @@
 function idade(){
-    
+    let idade;
+    do{
+        idade = parseInt(prompt("Informe sua idade (valores de 5 a 150): "));
+    } while ( idade < 5 || idade > 150 || idade != idade ); // ou Number.isNaN(idade)
+    alert("Idade validada.");
 }
---
+++ Urna/urna.js
@@ -0,0 +1,17 @@
+function urna(){
+    let candidato_a = 0;
+    let candidato_b = 0;
+    let brancos = 0;
+    let nulos = 0;
+    let total = 0;
+    let porvent_card_a;
+    let porcent_card_b;
+    let porcent_brancos;
+    let porcent_nulos;
+    let voto;
+
+    do{
+
+    }while( voto != 0);
+
+}
