a1d8041b9c68bdecb65342b4378dbaa5b64e7c0a
yunemaximo-png <yune270909@gmail.com>
2026-04-17 16:02:24 -0300

    Commit

--- alunissagem/jogo.js
+++ alunissagem/jogo.js
@@ -10,6 +10,19 @@ let canvas = document.querySelector("#jogo");
 let contexto = canvas.getContext("2d");
 const gravidade = 0.01;
 let lancamento = (Math.round(Math.random()) == 0);//Variável booleana pseudoaleatória
+let estrelas = [];
+
+for(let i = 0; i < 500 ; i++){
+    estrelas[i] = {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        raio: Math.sqrt(2 * Math.random()),
+        brilho: 1.0,
+        apagando: true,
+        cintilacao: 0.05 * Math.random()
+    }
+}
+
  
 let moduloLunar = {
     posicao: {
@@ -19,7 +32,7 @@ let moduloLunar = {
     angulo: lancamento ? -Math.PI/2 : Math.PI/2,
     largura: 20,
     altura: 20,
-    cor: "lightgray",
+    cor: "#647480",
     velocidade:{
         x: lancamento? 2: -2,
         y: 0,
@@ -36,7 +49,7 @@ function mostrarIndicador(mensagem, x, y){
     contexto.font = "bold 18px Arial";
     contexto.textAlign = "left";
     contexto.testBaseLine = "middle";
-    contexto.fillStyle = "lightgray";
+    contexto.fillStyle = "#6403ff";
     contexto.fillText(
         mensagem,
         x,
@@ -99,7 +112,34 @@ function desenharFundo(){
     contexto.fillRect(0,0, canvas.width, canvas.height);
     contexto.restore();
 }
- 
+
+function desenharEstrelas(){
+    contexto.save();
+    contexto.fillStyle = "#230332"
+    contexto.fillRect(0,0, canvas.width, canvas.height);
+    for(let i = 0; i < estrelas.length; i++){
+        let estrela = estrelas[i];
+        contexto.beginPath();
+        contexto.arc(estrela.x, estrela.y, estrela.raio, 0, 2*Math.PI);
+        contexto.closePath();
+        contexto.fillStyle = `rgba(255, 255, 255, ${estrela.brilho})`;
+        //contexto.fillStyle = "rgba(255, 255, 255, " + estrela.brilho + ")";
+        contexto.fill();
+        if(estrela.apagando){
+            estrela.brilho -= estrela.cintilacao;
+            if(estrela.brilho <= 0.1){
+                estrela.apagando = false;
+            }
+        } else {
+            estrela.brilho += estrela.cintilacao;
+            if(estrela.brilho > 0.95){
+                estrela.apagando = true;
+            }
+        }
+    }
+    contexto.restore();
+}
+
 function desenharModuloLunar(){
     contexto.save();
     contexto.beginPath();
@@ -113,7 +153,8 @@ function desenharModuloLunar(){
     contexto.closePath();
  
     if(moduloLunar.motorLigado){
-        desenharChama();
+        desenharChamaA();
+        desenharChamaB();
         consumirCombustivel();
     }
    
@@ -121,7 +162,7 @@ function desenharModuloLunar(){
  
 }
 
-function desenharChama(){
+function desenharChamaA(){
     contexto.beginPath();
     //define o ponto de origem da chama no canto inferior esquerdo do modulo
     contexto.moveTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
@@ -131,14 +172,28 @@ function desenharChama(){
     //desenha uma linha de posicao y aleatoria entre a base de modulo e 35 pixels
     contexto.lineTo(0, moduloLunar.altura *0.5 + Math.random() * 35);
     contexto.closePath();//desenha automaticamente a linha do vértice a origem
-    contexto.fillStyle = "orange";
+    contexto.fillStyle = "rgb(230, 109, 10)";
     contexto.fill();
 }
- 
+
+function desenharChamaB(){
+    contexto.beginPath();
+    //define o ponto de origem da chama no canto inferior esquerdo do modulo
+    contexto.moveTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
+    //desenha a linha até o ponto inferior direito do modulo
+    contexto.lineTo(moduloLunar.largura * 0.5, moduloLunar.altura * 0.5);
+    //determinar o tamanho da chama
+    //desenha uma linha de posicao y aleatoria entre a base de modulo e 35 pixels
+    contexto.lineTo(0, moduloLunar.altura * 0.5 + Math.random() * 25);
+    contexto.closePath();//desenha automaticamente a linha do vértice a origem
+    contexto.fillStyle = "#e0c410";
+    contexto.fill();
+}
 function desenhar(){
- 
-    atracaoGravitacional();
+
     desenharFundo();
+    desenharEstrelas();
+    atracaoGravitacional();
     desenharModuloLunar();
     mostrarVelocidadeVertical();
     mosrtarVelocidadeHorizontal();
@@ -146,6 +201,7 @@ function desenhar(){
     mostrarAltitude();
     mostrarAngulo();
     mostrarIndicador();
+    
     
     if(encerrarJogo()){
         return;
